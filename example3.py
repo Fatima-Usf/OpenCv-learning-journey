5442c6ce8a221ed602d0f67916325faded39e43a
Fatima-Usf <f.ucf.eva@gmail.com>
2019-11-01 01:18:38 +0100

    Added example4

--- example3.py
+++ example3.py
@@ -7,5 +7,5 @@ img = cv2.imread('testpic.jpg')
 
 img[50:200, 50:200] = (255,0,0)
 
-cv2.imshow("test", img) #affichage de mon image modifiee
-cv2.waitKey(0) # affichage de l'image tant que je n'appuie pas sur une touche
+cv2.imshow("test", img) #display of my modified image
+cv2.waitKey(0) #display the image as long as I do not press a key
---
+++ example4.py
@@ -0,0 +1,15 @@
+mport cv2
+
+image = cv2.imread('testpic.jpg')
+cropped = image[100:250, 200:350] #cropped take a part of our image
+
+image[100:200, 100:200] = (255,0,0) 
+image[300:350, 100:400] = (0,255,0)
+image[400:500, 100:200] = (0,0,255)
+
+cv2.imshow("test", image) 
+
+cv2.imwrite('copy.jpg', image) # saving our image with the name "copy.jpg"
+cv2.imwrite('cropped.jpg', cropped) 
+
+cv2.waitKey(0) 
