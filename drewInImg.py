4ee750631b959413120614e0045cd22dd959e367
Fatima-Usf <f.ucf.eva@gmail.com>
2019-11-04 22:09:23 +0100

    renaming

--- drewInImg.py
+++ drewInImg.py
@@ -5,7 +5,7 @@ import numpy as np
 
 img = cv2.imread('testpic.jpg') 
 
-img[50:200, 50:200] = (255,0,0)
+img[50:200, 50:200] = (255,0,0) # drew in the pixel from 50 to 200 with blue color
 
 cv2.imshow("test", img) #display of my modified image
 cv2.waitKey(0) #display the image as long as I do not press a key
