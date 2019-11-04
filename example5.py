ea066dfe53a5092458926ff0a1b971757afd45d0
Fatima-Usf <f.ucf.eva@gmail.com>
2019-11-04 21:58:33 +0100

    added the image  flipping effect

--- example5.py
+++ example5.py
@@ -3,6 +3,9 @@ import imutils
 
 """ Images Transformation """
 image = cv2.imread('testpic.jpg')
+image = cv2.flip(image,1) #fliping an image
+cv2.imshow('rotation', image)
+cv2.waitKey(0)
 
 rotated = imutils.rotate(image, 90) #Image rotation
 resized = imutils.resize(image, width = 300) #We defne here a new size to our image
@@ -10,8 +13,6 @@ translated = imutils.translate(image,100,0) #décalage de l'image
 
 
 cv2.imwrite('rotated.jpg', rotated) 
-cv2.imshow('rotation', image)
-cv2.waitKey(0)
 
 cv2.imwrite('resized.jpg', resized) 
 cv2.imwrite('translated.jpg', translated) 
